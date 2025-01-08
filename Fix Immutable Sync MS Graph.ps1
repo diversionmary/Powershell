4103623a392d82c22e5fcd9d51ed6093195a32de
diversionmary <73602834+diversionmary@users.noreply.github.com>
2025-01-08 10:34:26 -0800

    Narrow graph module, scope, and hard code URI

--- Fix Immutable Sync MS Graph.ps1
+++ Fix Immutable Sync MS Graph.ps1
@@ -1,8 +1,8 @@
 $UPN = Read-Host -Prompt 'Input the UPN'
-Import-Module Microsoft.Graph
-Connect-MgGraph -Scopes User.Read.All
+Import-Module Microsoft.Graph.Users
+Connect-MgGraph -Scopes User.ReadWrite.All
 Get-MgUser -UserId $UPN | Format-List UserPrincipalName, OnPremisesImmutableId
-Invoke-MgGraphRequest -Method PATCH -Uri "v1.0/users/$UPN" -Body @{onPremisesImmutableId = $null}
+Invoke-MgGraphRequest -Method PATCH -Uri "https://graph.microsoft.com/v1.0/users/$UPN" -Body @{onPremisesImmutableId = $null}
 for ($i = 1; $i -le 100; $i++ ) {
     Write-Progress -Activity "Fixing immutable sync" -Status "$i% Complete:" -PercentComplete $i
     Start-Sleep -Milliseconds 300
